0742b5666557ec918fc8e8d5dd39b141394d2196
Conor Ward <sirconorjamesward@gmail.com>
2021-08-10 14:49:31 -0600

    3.6

--- index.js
+++ index.js
@@ -39,12 +39,40 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id',(request, response) => {
   const id = Number(request.params.id)
-  const persons = phonebook.find(persons => persons.id === id)
-  if (persons){
-    response.json(persons)
+  const person = phonebook.find(person => person.id === id)
+  if (person){
+    response.json(person)
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id',(request, response) => {
+  const id = Number(request.params.id)
+  phonebook = phonebook.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons',(request, response) => {
+  const body = request.body
+
+  if(!body.name || !body.number){
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  } else if (phonebook.some(person => person.name === body.name)){
+    return response.status(400).json({ 
+      error: 'name already in phonebook' 
+    })
+  }
+
+  const person = {
+    id: Math.floor(Math.random() * 1000000),// x in 1,000,000 chance of getting duplicate id
+    name: body.name,
+    number: body.number
+  }
+  phonebook = phonebook.concat(person)
+  response.json(phonebook)
 })
 
 const PORT = 3001
